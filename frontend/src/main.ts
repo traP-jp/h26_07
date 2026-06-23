a0773fc06cb227b4daccd0c9f8b5abd784fcd33e
mumumu <takumu.sato@gmail.com>
2026-06-24 00:33:16 +0900

    chore : nuxt-uiをインストール

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ui from '@nuxt/ui/vue-plugin'
 
+import './assets/css/main.css'
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +10,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ui)
 
 app.mount('#app')
