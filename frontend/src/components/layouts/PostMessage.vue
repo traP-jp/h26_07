01b3dbc6c0fa6aa40e1ccc50213f62eb4a836973
Hokubu Subway <hokubusub@gmail.com>
2026-06-27 16:19:42 +0900

    メッセージ投稿時に API を叩く機能が追加されました

--- frontend/src/components/layouts/PostMessage.vue
+++ frontend/src/components/layouts/PostMessage.vue
@@ -1,11 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const roomId = defineProps<{ roomId: string }>()
+const newMessage = ref('')
+type RequestBody = {
+  content: string
+}
+const post = async () => {
+  const data: RequestBody = {
+    content: newMessage.value,
+  }
+  try {
+    const response = await fetch(`/api/rooms/${roomId}/chats`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`)
+    }
+    const result = await response.json()
+    console.log('Success', result)
+  } catch (error) {
+    console.error('Error', error)
+  }
+}
+</script>
 
 <template>
   <div style="display: flex; gap: 6px">
     <div class="newMessage">
-      <input id="newMessage" type="text" />
+      <input id="newMessage" v-model="newMessage" type="text" />
     </div>
-    <div class="button">
+    <div class="button" @click="post()">
       <svg height="40" width="40" view-box="0 0 40 40">
         <path d="M0 0v17l40 3-40 3v17l40-20z"></path>
       </svg>
