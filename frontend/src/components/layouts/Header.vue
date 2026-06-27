70afded2864678fd02e3f3efb130492455cd61a3
rurun <107459257+rurun418@users.noreply.github.com>
2026-06-27 15:34:44 +0900

    Headerを作成

--- frontend/src/components/layouts/Header.vue
+++ frontend/src/components/layouts/Header.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import { useCurrentUserStore } from '@/stores/currentUser'
+import { computed } from 'vue'
+
+const currentUserStore = useCurrentUserStore()
+const userId = computed(() => currentUserStore.userId)
+const iconUrl = computed(() => `https://q.trap.jp/api/v3/public/icon/${userId.value}`)
+</script>
+
 <template>
-  <header>
-    <h1>Header</h1>
-  </header>
+  <UHeader title="BINGO ONLINE" :toggle="false">
+    <template #right>
+      <UButton icon="i-lucide-plus" to="./new" target="_blank">新しいルームを作成</UButton>
+      <UAvatar :src="iconUrl" loading="lazy" />
+      <div>{{ userId }}</div>
+    </template>
+  </UHeader>
 </template>
