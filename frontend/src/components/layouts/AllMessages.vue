6d47fea68feb8a88bd91d9ae471ebcb1c966a04c
Hokubu Subway <hokubusub@gmail.com>
2026-06-27 19:18:19 +0900

    WebSocket 反映

--- frontend/src/components/layouts/AllMessages.vue
+++ frontend/src/components/layouts/AllMessages.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import type { Uuid, Message, DateTime } from '@/api/schema'
-import {} from '@/stores/roomWebSocket'
+import { useRoomWebSocketStore } from '@/stores/roomWebSocket'
 const room = defineProps<{ roomId: string; textarea: boolean }>()
 const messages = ref<Message[]>([])
 
@@ -18,7 +18,57 @@ const addSpecialMessage = (id: Uuid, content: string, createdAt: DateTime) => {
   })
 }
 
-defineExpose({ addUserMessage, addSpecialMessage })
+const store = useRoomWebSocketStore()
+
+watch(
+  () => store.latestMessage,
+  (newValue) => {
+    if (newValue) {
+      addUserMessage(newValue)
+    }
+  },
+)
+watch(
+  () => store.pickState,
+  (newValue) => {
+    if (newValue == 'exhausted') {
+      addSpecialMessage('allPicked', '球が枯渇しました！', 'ima')
+    }
+  },
+)
+watch(
+  () => store.latestNewBingos,
+  (newValue) => {
+    if (newValue) {
+      if (newValue.length >= 2) {
+        addSpecialMessage(
+          'newBingos',
+          `${newValue[0]?.user} と他 ${newValue.length - 1} 人がビンゴしました！`,
+          'ima',
+        )
+      } else {
+        addSpecialMessage('newBingos', `${newValue[0]?.user} がビンゴしました！`, 'ima')
+      }
+    }
+  },
+)
+watch(
+  () => store.latestNewReaches,
+  (newValue) => {
+    if (newValue) {
+      if (newValue.length >= 2) {
+        addSpecialMessage(
+          'newReaches',
+          `${newValue[0]?.user} と他 ${newValue.length - 1} 人がリーチしました！`,
+          'ima',
+        )
+      } else {
+        addSpecialMessage('newReaches', `${newValue[0]?.user} がリーチしました！`, 'ima')
+      }
+    }
+  },
+)
+defineExpose({ addSpecialMessage })
 </script>
 
 <template>
