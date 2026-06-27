0aa506279c73daae50bfd4d50ed3934e58b800b1
kurostp <aosushi828@gmail.com>
2026-06-27 12:12:38 +0900

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,4 @@
-<script setup lang="ts">
-import BallIcon from './components/layouts/number_balls.vue'
-</script>
 <template>
-  <BallIcon ball-color="red" text-color="black" text="33" size="48px"> </BallIcon>
   <UApp>
     <RouterView />
   </UApp>
