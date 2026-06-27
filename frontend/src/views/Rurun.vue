f3a916ab2a3bef074ce64f0e80ee2d50ed4f829d
rurun <107459257+rurun418@users.noreply.github.com>
2026-06-27 11:57:41 +0900

    プレビュー画面の作成

---
+++ frontend/src/views/Rurun.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Preview</h2>
+    <div class="p-6">
+      <Card
+        title="春ハッカソン2026"
+        description="優勝者には豪華賞品をプレゼント！"
+        room-code="123456"
+      ></Card>
+    </div>
+  </div>
+</template>
