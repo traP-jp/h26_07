bbb375d11022212c4cdb3ff1631c8f8a2bb575f6
kurostp <aosushi828@gmail.com>
2026-06-28 14:03:45 +0900

    モーダル設定

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,34 +1,58 @@
 <script setup lang="ts">
 import { computed } from 'vue'
+
 interface Props {
   title: string
   description: string
   roomCode: string
   isAdmin?: boolean
+  isJoinable?: boolean
 }
+
 const props = withDefaults(defineProps<Props>(), {
   isAdmin: false,
+  isJoinable: false,
 })
-const joinUrl = computed(() => `./${props.roomCode}/participant`)
-const displayUrl = computed(() => `./${props.roomCode}/display`)
-const settingsUrl = computed(() => `./${props.roomCode}/settings`)
-const controllerUrl = computed(() => `./${props.roomCode}/controller`)
+
+const joinUrl = computed(() => `/${props.roomCode}/participant`)
+const displayUrl = computed(() => `/${props.roomCode}/display`)
+const settingsUrl = computed(() => `/${props.roomCode}/settings`)
+const controllerUrl = computed(() => `/${props.roomCode}/controller`)
+
+const emit = defineEmits<{
+  join: []
+}>()
 </script>
 
 <template>
   <UCard class="w-full p-1" variant="outline" :ui="{ body: 'p-2 sm:p-2' }">
-    <h3 class="font-bold text-2xl">{{ title }}</h3>
-    <p>{{ description }}</p>
+    <h3 class="font-bold text-2xl">{{ props.title }}</h3>
+    <p>{{ props.description }}</p>
+
     <div class="flex gap-2 mt-2 flex-wrap">
       <UButton
+        v-if="props.isJoinable"
+        @click.stop.prevent="emit('join')"
+        color="primary"
+        variant="solid"
+        icon="i-lucide-rocket"
+        size="md"
+      >
+        ゲームに参加
+      </UButton>
+
+      <UButton
+        v-else
         color="primary"
         variant="solid"
         icon="i-lucide-rocket"
         size="md"
         :to="joinUrl"
         target="_blank"
-        >ゲームに参加</UButton
       >
+        ゲームに参加
+      </UButton>
+
       <UButton
         color="primary"
         variant="soft"
@@ -36,28 +60,33 @@ const controllerUrl = computed(() => `./${props.roomCode}/controller`)
         size="md"
         :to="displayUrl"
         target="_blank"
-        >表示用画面</UButton
       >
+        表示用画面
+      </UButton>
+
       <UButton
+        v-if="props.isAdmin"
         color="primary"
         variant="outline"
         icon="i-lucide-settings"
         size="md"
         :to="settingsUrl"
         target="_blank"
-        v-if="isAdmin"
-        >設定</UButton
       >
+        設定
+      </UButton>
+
       <UButton
+        v-if="props.isAdmin"
         color="primary"
         variant="outline"
         icon="i-lucide-gamepad"
         size="md"
         :to="controllerUrl"
         target="_blank"
-        v-if="isAdmin"
-        >操作</UButton
       >
+        操作
+      </UButton>
     </div>
   </UCard>
 </template>
